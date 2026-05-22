fdd2f5171a187a0b434e3ffd8b3c0b013cbb7c70
Anthalk <anthony26khouni@gmail.com>
2026-05-22 09:02:08 +0200

    fixed globe position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -42,7 +42,7 @@ const Map = forwardRef(function Map({ etapes = [], spots = [], routes = {}, tigh
       container: mapContainer.current,
       style: maptilersdk.MapStyle.HYBRID_V4,
       center: [20, 30],
-      zoom: 0,
+      zoom: 1,
       zoomControl: false,
       projection : 'globe',
       navigationControl: false,
